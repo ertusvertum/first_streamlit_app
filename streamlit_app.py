caa7dc48e1336021d6220df31d3733efd30c736d
ertusvertum <133984192+ertusvertum@users.noreply.github.com>
2023-05-19 16:07:49 +0800

    Update streamlit_app.py
    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ except URLError as e:
   streamlit.error()
 
 def get_fruit_load_list():
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     with my_cnx.cursor as my_cur:
         my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
         return my_cur.fetchall()
@@ -49,7 +50,6 @@ def get_fruit_load_list():
 streamlit.header("The Fruit Load lIst contains:")
 
 if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
