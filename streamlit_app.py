bd757a39890a576462e2263da0db284caa116764
ertusvertum <133984192+ertusvertum@users.noreply.github.com>
2023-05-19 15:20:11 +0800

    Update streamlit_app.py
    add and edit line to add row in table

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,3 +47,4 @@ streamlit.text(my_data_row)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
